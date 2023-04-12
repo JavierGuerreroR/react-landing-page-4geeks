bee8e7e570adba6d153c5d7eea8b38eb2f03585b
JavierGuerreroR <javierguerrerorosas@gmail.com>
2023-04-12 19:16:42 +0000

    cards in images off

--- src/js/component/card.jsx
+++ src/js/component/card.jsx
@@ -1,9 +1,9 @@
 import React from "react";
-function Card(){
+const Card = () => {
     return(
     <div class="card" style={{width: '18rem'}}>
+        <img src="/workspace/react-hello/src/img/485x325.png" class="card-img-top"/>
         <div class="card-body">
-            <img src="/workspace/react-hello/src/img/485x325.png" class="card-img-top"/>
             <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
             <a href="#" class="btn btn-primary">Go somewhere</a>
         </div>
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -3,6 +3,7 @@ import React from "react";
 //include images into your bundle
 import Nav from "/src/js/component/nav.jsx"
 import Jumbotron from "./jumbotron.jsx";
+import Card from "./card.jsx";
 import Footer from "./footer.jsx";
 //create your first component
 const Home = () => {
@@ -10,15 +11,19 @@ const Home = () => {
 		<div className="text-center">
 			<Nav/>
 			<Jumbotron/>
-			<div class="container text-center">
-				<div class="row align-items-start">
-					<div class="col">
+			<div className="container text-center">
+				<div className="row align-items-start">
+					<div className="col col-3">
+						<Card/>
 					</div>
-					<div class="col">
-					cartaaaaa
+					<div className="col col-3">
+						<Card/>
 					</div>
-					<div class="col">
-					One of three columns
+					<div className="col col-3">
+						<Card/>
+					</div>
+					<div className="col col-3">
+						<Card/>
 					</div>
 				</div>
 			</div>
